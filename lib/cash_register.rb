7fb31e4ac3cc11863d63c718e21b95bee6363d3f
Lu Daley <daleyluis@gmail.com>
2019-02-22 16:30:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,6 @@ class CashRegister
      @total += price * quantity
    end
    def apply_discount
-     @total = @total - (@total * (@discount/100))
+     @total = @total - (@total * (@discount/100.00))
    end
 end
